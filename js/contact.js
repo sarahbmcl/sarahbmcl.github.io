b9f14dcb1c41b492988daaa079026b668ccf609e
Sarah McLaughlin <sarahbmcl@gmail.com>
2020-02-03 23:53:08 -0600

    contact updates--nonworking

---
+++ js/contact.js
@@ -0,0 +1,49 @@
+let fields={};
+
+document.addEventListener("DOMContentLoaded", function() {
+    fields.firstName = document.getElementById('firstName');
+    fields.lastName = document.getElementById('lastName');
+    fields.email = document.getElementById('email');
+    fields.phone = document.getElementById('phone');
+    fields.textarea1 = document.getElementById('textarea1');
+})
+
+function isNotEmpty(value) {
+if (value == null || typeof value == 'undefined' ) return false;
+return (value.length > 0);
+}
+
+function isNumber(num) {
+    return (num.length > 0) && !isNaN(num);
+}
+
+function isEmail(email) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+}
+
+function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.firstName, isNotEmpty);
+    valid &= fieldValidation(fields.lastName, isNotEmpty);
+    valid &= fieldValidation(fields.textarea1, isNotEmpty);
+    valid &= fieldValidation(fields.phone, isNumber);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= arePasswordsEqual();
+   
+    return valid;
+}
